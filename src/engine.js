73f3414068b6b3b0c7f41f1d5d46a9a5d772cb76
William Lew <WilliamMilesLew@gmail.com>
2019-05-13 00:11:11 -0700

    added conversion from (R,G,B,A) to float

--- src/engine.js
+++ src/engine.js
@@ -1,3 +1,4 @@
+const MAX_DATA_VALUE = 5599.9999;
 const MAX_RGB_VALUES = [255,99,99,99];
 const SCREEN_WIDTH = 200;
 const SCREEN_HEIGHT = 200;
@@ -10,8 +11,8 @@ var currentDataCoord = [0,0];
 function initialize() {
 	var canvas = document.getElementById("glCanvas");
 	gl = canvas.getContext("experimental-webgl");
-	gl.enable(gl.BLEND);
-	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+	// gl.enable(gl.BLEND);
+	// gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
 	programList.push(createProgram(VERTEX_SHADER_1, FRAGMENT_SHADER_1));
@@ -19,13 +20,15 @@ function initialize() {
 	gl.clearColor(0.0, 0.0, 0.0, 0.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	for (var x = 0; x < 1000; x++) { addData(59.12); }
+	for (var x = 0; x < 1; x++) { addData(1235.5555); }
 
 	pixelDataList.forEach(pixel => { pixel.draw(); });
+	console.log(convertPixelToData(getPixel(0,0)));
 }
 
 function addData(data) {
-	var rgba = convertData(data);
+	var rgba = convertDataToPixel(data);
+	console.log(rgba);
 	pixelDataList.push(new Point(currentDataCoord[0], currentDataCoord[1], [rgba[0],rgba[1],rgba[2],rgba[3]]));
 
 	if (currentDataCoord[0] == gl.canvas.width - 1) {
@@ -35,12 +38,12 @@ function addData(data) {
 	else { currentDataCoord[0] += 1; }
 }
 
-function convertData(data) {
+function convertDataToPixel(data) {
 	if (data > 5599.9999) { return ([155,99,99,99]); }
 	if (data < -5599.9999) { return ([255,99,99,99]); }
 
 	var tempData = Math.abs(data);
-	var dec = Math.floor((tempData % 1) * 10000);
+	var dec = Math.round((tempData % 1) * 10000);
 	tempData = Math.floor(tempData);
 	var g = tempData % 100;
 	var r = Math.floor(tempData / 100);
@@ -49,6 +52,14 @@ function convertData(data) {
 
 	if (data < 0) { return ([r + 200, g, b, a]); }
 	else { return ([r + 100, g, b, a]); }
+}
+
+function convertPixelToData(pixel) {
+	var tempData = (pixel[0] - (Math.floor(pixel[0] / 100) * 100)) * 100;
+	tempData += Math.floor(pixel[1]/255.0*100.0) + (Math.floor((pixel[2]/255)*100)/100) + (Math.floor((pixel[3]/255*100))/10000);
+
+	if (Math.floor(pixel[0] / 100) == 1) { return tempData; }
+	if (Math.floor(pixel[0] / 100) == 2) { return -tempData; }
 }
 
 function getPixel(x, y) {
