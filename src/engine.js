600ce9f011a02d7989d436cb0ab16aec7fb48067
William Lew <WilliamMilesLew@gmail.com>
2019-05-20 13:48:38 -0700

    generalized the library to start with no default PixelBuffer

--- src/engine.js
+++ src/engine.js
@@ -16,20 +16,9 @@ function initializeFC(canvas = document.createElement('canvas')) {
 	programListFC.push(createProgramFC(VERTEX_SHADER_DEFAULT, FRAGMENT_SHADER_DEFAULT));
 	programListFC.push(createProgramFC(VERTEX_SHADER_PIXEL_BUFFER, FRAGMENT_SHADER_PIXEL_BUFFER));
 	programListFC.push(createProgramFC(VERTEX_SHADER_MULTIPLY_MATRIX, FRAGMENT_SHADER_MULTIPLY_MATRIX));
-	
-	performOperationsFC();
-}
 
-function performOperationsFC() {
 	glFC.clearColor(0.0, 0.0, 0.0, 0.0);
 	glFC.clear(glFC.COLOR_BUFFER_BIT | glFC.DEPTH_BUFFER_BIT);
-	
-	var pixelBuffer = new PixelBuffer();
-	pixelBuffer.pushMatrix([0,1,2,3,4,5], 2, 3);
-	pixelBuffer.pushMatrix([4,5,6,7,8,9], 3, 2);
-	pixelBuffer.render();
-	pixelBuffer.multiplyMatrix(0,1);
-	console.log(pixelBuffer.getMatrix(2));
 }
 
 function createProgramFC(vertexSource, fragmentSource) {
